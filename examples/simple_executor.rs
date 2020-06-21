01d1c6eb9a7024e423d74fd1607ebe5aa1a1d888
Tyler Holmes <tylerdanielholmes@gmail.com>
2020-06-20 17:15:44 -0700

    add more complex executor

--- examples/simple_executor.rs
+++ examples/simple_executor.rs
@@ -18,12 +18,16 @@ use panic_halt as _;
 
 use core::alloc::Layout;
 
+use alloc::rc::Rc;
+use alloc::boxed::Box;
+use core::cell::Cell;
+
 use alloc_cortex_m::CortexMHeap;
-use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 
 use futures::future::lazy;
+use futures::task::LocalSpawn;
 use embedded_futures::CortexMExecutor;
 
 
@@ -31,21 +35,31 @@ use embedded_futures::CortexMExecutor;
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
-const HEAP_SIZE: usize = 1024; // in bytes
+const HEAP_SIZE: usize = 1024 * 2; // in bytes
 
 #[entry]
 fn main() -> ! {
     // Initialize the allocator BEFORE you use it
     unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
 
-    let mut cnt = 0;
     let mut pool = CortexMExecutor::new();
-    let fut = lazy(|_| {
-        cnt += 1;
-    });
-    pool.run_until(fut);
 
-    if cnt == 1 {
+    const ITER: usize = 20;
+
+    let cnt = Rc::new(Cell::new(0));
+
+    let spawn = pool.spawner();
+
+    for _ in 0..ITER {
+        let cnt = cnt.clone();
+        spawn.spawn_local_obj(Box::pin(lazy(move |_| {
+            cnt.set(cnt.get() + 1);
+        })).into()).unwrap();
+    }
+
+    pool.run();
+
+    if cnt.get() == ITER {
         hprintln!("future worked!").unwrap();
     } else {
         hprintln!("something went wrong...").unwrap();
@@ -61,7 +75,8 @@ fn main() -> ! {
 // define what happens in an Out Of Memory (OOM) condition
 #[alloc_error_handler]
 fn alloc_error(_layout: Layout) -> ! {
-    asm::bkpt();
+    hprintln!("ALLOC ERR").unwrap();
+    debug::exit(debug::EXIT_FAILURE);
 
     loop {}
 }
